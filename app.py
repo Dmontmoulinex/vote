5ef764a01b78a873eaebd5691ba757cd10de5788
Dmontmoulinex <montmoulinex.damien@gmail.com>
2024-10-31 07:58:49 +0000

    catwoman and batman

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', "Catwoman")
+option_b = os.getenv('OPTION_B', "Batman")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
